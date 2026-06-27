47d913b202b608787cf06701d068a9216d848bbc
harshavardan1403 <harshavardan1403@gmail.com>
2026-06-27 14:42:29 +0000

    commit on updating import check file

--- installation_check.py
+++ installation_check.py
@@ -4,4 +4,7 @@ import matplotlib as mpl
 import numpy as np
 
 print("\nImport Check")
-print("")
+print("Version of MEEP:",mp.__version__)
+print("Version of Matplotlib:",mpl.__version__)
+print("Version of Numpy:",np.__version__)
+print("All imports are working successfully")
